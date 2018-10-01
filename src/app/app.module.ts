4adaa3e51ccf09dcb6d79690767bcd3b53dd4f6d
Jakub Nurski <nurskijakub@gmail.com>
2018-10-01 20:03:49 +0200

    Styled home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { MdButtonModule, MdChipsModule, MdListModule } from "@angular/material";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { EffectsModule } from "@ngrx/effects";
@@ -8,20 +9,26 @@ import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 import { environment } from "../environments/environment";
 import { AppComponent } from "./app.component";
+import { routes } from "./app.routes";
+import { HomeComponent } from "./components/home/home.component";
 import { RouterEffects } from "./effects/router";
 import { CustomRouterStateSerializer, metaReducers, reducers } from "./reducers";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     StoreModule.forRoot(reducers, { metaReducers }),
-    RouterModule.forRoot([]),
+    RouterModule.forRoot(routes),
     StoreRouterConnectingModule,
     !environment.production
       ? StoreDevtoolsModule.instrument({ maxAge: 30 })
       : [],
-    EffectsModule.forRoot([RouterEffects])
+    EffectsModule.forRoot([RouterEffects]),
+    
+    MdButtonModule,
+    MdListModule,
+    MdChipsModule
   ],
   providers: [
     { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer }
